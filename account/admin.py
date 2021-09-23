6eaa65f018fc123f646e857b6a0ceac29adcc399
mohammadhssn <mohammadhssnalizadeh78@gmail.com>
2021-09-23 18:27:19 +0330

    Extend User Admin with add model Profile and use signal

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.db.models.signals import post_save
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
+def SaveProfile(sender, **kwargs):
+    if kwargs.get('created'):
+        p1 = Profile(user=kwargs.get('instance'))
+        p1.save()
+
+
+post_save.connect(receiver=SaveProfile, sender=User)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200, null=True, blank=True)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
